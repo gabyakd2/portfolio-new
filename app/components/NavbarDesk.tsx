c1041b882b8a1711c231679bf87754b9b7505dd7
gabyakd2 <gabrielsal00@hotmail.com>
2024-03-12 02:03:51 -0300

    add: icons added

--- app/components/NavbarDesk.tsx
+++ app/components/NavbarDesk.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Link from "next/link";
-import { Switch } from "@/components/ui/switch"
-
+import { FaMoon, FaSun } from "react-icons/fa";
+import { Switch } from "@/components/ui/switch";
 
 function NavbarDesk() {
   return (
     <nav className="py-6 border-b border-solid flex justify-between">
-      <div className="font-semibold space-x-3 flex text-white">
+      <div className="font-semibold space-x-8 flex text-white">
         <Link href="/">Inicio</Link>
         <Link href="/projects">Proyectos</Link>
         <Link href="/about-me">Acerca de mí</Link>
       </div>
-      <Switch />
+      <div className="flex space-x-3 items-center">
+        <FaMoon style={{ color: "white", fontSize: "1em" }} />
+        <Switch />
+        <FaSun style={{ color: "white", fontSize: "1em" }}/>
+      </div>
     </nav>
   );
 }
