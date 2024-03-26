5608adeb86671b71e90f41b12e2951ec1ce68758
gabyakd2 <gabrielsal00@hotmail.com>
2024-03-26 11:29:22 -0300

    add: contact section added

--- app/(pages)/home/components/contact/Contact.jsx
+++ app/(pages)/home/components/contact/Contact.jsx
@@ -1,12 +1,39 @@
+import Link from "next/link";
 import React from "react";
 
 function Contact() {
   return (
     <div className="text-white">
       <p className="text-3xl font-semibold">Contacto 📩</p>
-      <p>Para contactarme, no dudes en hacerlo por Linkedin o por email</p>
+      <p>
+        Para contactarme, no dudes en hacerlo por{" "}
+        <Link
+          href="https://www.linkedin.com/in/gabriel-saldana00/"
+          className="text-greenpf font-semibold underline"
+        >
+          Linkedin
+        </Link>{" "}
+        o por{" "}
+        <Link
+          href="mailto:gabrielsal00@hotmail.com"
+          className="text-greenpf font-semibold underline"
+        >
+          correo
+        </Link>
+      </p>
       <div className="">
-        <p>Para descargar mi CV:</p>
+        <p>
+          Para{" "}
+          <Link
+            href="./cv.pdf"
+            className="text-greenpf font-semibold underline"
+            download="Gabriel Saldaña CV"
+            target="_blank"
+          >
+              descargar
+          </Link>{" "}
+          mi CV
+        </p>
       </div>
     </div>
   );
