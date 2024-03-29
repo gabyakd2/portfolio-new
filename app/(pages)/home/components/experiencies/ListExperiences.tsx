f5b255f6a06c056928229973ea6198357db29eff
gabyakd2 <gabrielsal00@hotmail.com>
2024-03-28 21:39:26 -0300

    fix: card list responsive

--- app/(pages)/home/components/experiencies/ListExperiences.tsx
+++ app/(pages)/home/components/experiencies/ListExperiences.tsx
@@ -1,31 +1,35 @@
 import React from "react";
-import experiences from "../../../../mockExperiende.json"
+import experiences from "../../../../mockExperiende.json";
 import CardExperience from "./CardExperience";
 
 interface IExperiences {
-  company: string,
-  position: string,
-  time: string,
-  description: string,
-  skills: string[]
-  image?: string
+  company: string;
+  position: string;
+  time: string;
+  description: string;
+  skills: string[];
+  image?: string;
 }
 
 function ListExperiences() {
   return (
     <div className="mt-20">
       <h3 className="text-3xl text-white font-semibold mb-10">Experiencias</h3>
-    <div className="grid grid-cols-2 gap-5">
-      {
-        experiences.map(({company, position, time, description, skills}) => (
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
+        {experiences.map(({ company, position, time, description, skills }) => (
           <div key={company}>
-            <CardExperience company={company} position={position} time={time} description={description} skills={skills}/>
+            <CardExperience
+              company={company}
+              position={position}
+              time={time}
+              description={description}
+              skills={skills}
+            />
           </div>
-        ))
-      }
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default ListExperiences
+export default ListExperiences;
